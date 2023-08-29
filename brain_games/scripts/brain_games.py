f538e085f3938c546a5866ddf0e9661795713503
Vladislav <1@zithen.com>
2023-08-29 18:26:50 +0300

    Add welcome_user func call

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,5 @@
  #!/usr/bin/env python
+from brain_games.cli import welcome_user
 
 
 def welcome():
@@ -7,8 +8,8 @@ def welcome():
 
 def main():
     welcome()
+    welcome_user()
 
 
 if __name__ == '__main__':
     main()
-    
