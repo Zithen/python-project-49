a11f140fb94c98143a0bcda72f9eef0e58145c24
Vladislav <1@zithen.com>
2023-08-29 19:29:08 +0300

    Finish games engine

--- brain_games/games/engine.py
+++ brain_games/games/engine.py
@@ -8,6 +8,21 @@ def engine(game):
     user_name = string('May I have your name? ')
     print(f'Hello, {user_name}')
 
+    print(game.get('rules'))
+
     for _ in range(questions_to_ask):
+        question = game.get('question')
         correct_answer = game.get('correct_answer')
         user_answer = game.get('user_answer')
+
+        print(question)
+
+        if correct_answer != user_answer:
+            print(f'{user_answer} is wrong answer ;(.', end=' ')
+            print(f'Correct answer was {correct_answer}.')
+            print(f"Let's try again, {user_name}!")
+            return 1
+        else:
+            print('Correct!')
+
+    print(f'Congratulations, {user_name}!')
