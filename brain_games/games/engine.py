2bb4f74dd30bb33ea136f08d3c82f7f873d5029c
Vladislav <1@zithen.com>
2023-08-29 23:48:18 +0300

    Add acript brain_even

--- brain_games/games/engine.py
+++ brain_games/games/engine.py
@@ -1,18 +1,18 @@
 from prompt import string
 
 
-def engine(game):
+def engine(game_params_dict):
     questions_to_ask = 3
     print('Welcome to the Brain Games!')
 
     user_name = string('May I have your name? ')
     print(f'Hello, {user_name}')
 
-    print(game.get('rules'))
+    print(game_params_dict.get('rules'))
 
     for _ in range(questions_to_ask):
-        question = game.get('question')
-        correct_answer = game.get('correct_answer')
+        question = game_params_dict.get('question')
+        correct_answer = game_params_dict.get('correct_answer')
 
         print(f'Question: {question}')
         user_answer = string('Your answer: ')
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+from brain_games.games.engine import engine, even_game
+
+
+def main():
+    engine(even_game())
+
+
+if __name__ == '__main__':
+    main()
