369145b3c5e61d8016150245af274c5a10334e29
Vladislav <1@zithen.com>
2023-08-30 17:17:23 +0300

    dd

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.scripts import brain_even
+from brain_games.games.engine import engine
 from brain_games.games import calc_game
 
 
 def main():
-    brain_even.engine(calc_game)
+    engine(calc_game)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.scripts import brain_calc
+from brain_games.games.engine import engine
 from brain_games.games import gcd_game
 
 
 def main():
-    brain_calc.engine(gcd_game)
+    engine(gcd_game)
 
 
 if __name__ == '__main__':
