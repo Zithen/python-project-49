888809144d8c14f663d0577d8f9bf2356ee505aa
Vladislav <1@zithen.com>
2023-08-30 16:17:49 +0300

    Hide elem in progression list

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -8,8 +8,11 @@ def question():
     progression_first_elem = randint(1, 10000)
     progression_list = [progression_first_elem]
     progression_step = randint(1, 10000)
+    element_to_hide = randint(0, 9)
     for _ in range(1, 10):
         progression_list.append(progression_list[_ - 1] + progression_step)
+
+    progression_list[element_to_hide] = '..'
 
     return ' '.join(str(elem) for elem in progression_list)
 
