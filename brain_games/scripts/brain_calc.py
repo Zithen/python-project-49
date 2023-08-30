3e93bdd02b75a85664821298d6d3415abfb2647b
Vladislav <1@zithen.com>
2023-08-30 17:07:39 +0300

    Refactor scripts

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.engine import engine
+from brain_games.scripts import brain_even
 from brain_games.games import calc_game
 
 
 def main():
-    engine(calc_game)
+    brain_even.engine(calc_game)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.engine import engine
+from brain_games.scripts import brain_calc
 from brain_games.games import gcd_game
 
 
 def main():
-    engine(gcd_game)
+    brain_calc.engine(gcd_game)
 
 
 if __name__ == '__main__':
