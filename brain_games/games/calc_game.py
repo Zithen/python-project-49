2b162895e16d3d20f18596939004fc69e6b098d8
Vladislav <1@zithen.com>
2023-08-31 13:25:54 +0300

    Change calc_game logic for new engine

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -4,17 +4,12 @@ from random import randint, choice
 RULES = 'What is the result of the expression?'
 
 
-def question():
-    operand1 = randint(1, 1000)
-    operand2 = randint(1, 1000)
+def game_logic():
+    operand1 = randint(1, 10)
+    operand2 = randint(1, 10)
     operator = choice(['+', '-', '*'])
 
     expression = str(operand1) + ' ' + operator + ' ' + str(operand2)
+    correct_answer = eval(expression)
 
-    return expression, None
-
-
-def game(question):
-    correct_answer = eval(question)
-
-    return str(correct_answer)
+    return expression, str(correct_answer)
