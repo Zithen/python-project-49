9b58381512c16d680ea9ae19ddd4ff9b232f4f6b
Vladislav <1@zithen.com>
2023-08-31 14:48:29 +0300

    Rename vars and add comments

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -7,11 +7,12 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def game_logic():
     num = randint(1, 3571)
-    question = ceil(num / 2)
-    # if num is 1 or 2 will avoid cycle
+    # no sense to check num / > half of num as it will give remainder
+    upper_limit_divisor = ceil(num / 2)
+    # if num is 1 or 2 will avoid cycle below and miss the returned var value
     correct_answer = 'no' if num == 1 else 'yes'
 
-    for divisor in range(question, 1, -1):
+    for divisor in range(upper_limit_divisor, 1, -1):
         if num % divisor == 0:
             correct_answer = 'no'
             break
